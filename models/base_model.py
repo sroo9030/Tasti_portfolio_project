c50704ac59db374781b12f3fdc1e6b436b7a3829
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-27 19:34:33 +0300

    base model updated

---
+++ models/base_model.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+basemodel identification
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from uuid import uuid4
+from sqlalchemy import Column, String, DateTime
+from models import storage
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """BaseModel Class"""
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        if kwargs:
+            tt = '%Y-%m-%dT%H:%M:%S.%f'
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    setattr(self, k, v)
+            if kwargs.get('created_at') and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs['created_at'], tt)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get('updated_at') and type(kwargs['updated_at']) is str:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], tt)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get('id') is None:
+                self.id = str(uuid4())
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+
+    def save(self):
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.utcnow()
+        storage.new(self)
+        storage.save()
+
+    def delete(self):
+        """delete from storage"""
+        storage.delete(self)
