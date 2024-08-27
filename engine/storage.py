afbe226d998c0293f7e82494975225dd4ed9bf0c
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-27 19:35:16 +0300

    engine created

---
+++ engine/storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+database storage operations
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from uuid import uuid4
+from sqlalchemy import Column, String, DateTime
