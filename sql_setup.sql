a44f036673acc49579ca2fe645a6e52647edd619
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-08-29 22:17:08 +0300

    database setup

---
+++ sql_setup.sql
@@ -0,0 +1,7 @@
+-- setting up mysql server
+
+CREATE DATABASE IF NOT EXISTS tasti_db;
+CREATE USER IF NOT EXISTS 'tasti_user'@'localhost' IDENTIFIED BY 'tasti_pwd';
+GRANT ALL PRIVILEGES ON `tasti`.* TO 'tasti_user'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'tasti_user'@'localhost';
+FLUSH PRIVILEGES;
