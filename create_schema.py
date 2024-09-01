ef3a35b4d61eb94b613f292f25cca2f0b9cfe27d
sozoia <zfzg2232@gmail.com>
2024-09-02 02:00:10 +0300

    script for creating the schema

--- create_schema.py
+++ create_schema.py
@@ -4,9 +4,13 @@ create the tables from my models
 '''
 
 from models import db,app
+from models.user import User 
+from models.review import Review
+from models.recipe import Recipe
 
 with app.app_context():
     ''' access the app_context allow to intract with app and get required config'''
 
     # create the tables again and the strucure of my models:
+    db.drop_all()
     db.create_all()
