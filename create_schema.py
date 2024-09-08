66bfcec290e5adaa61f224e9b70b1ee2e4bc212d
sozoia <zfzg2232@gmail.com>
2024-09-08 18:44:15 +0300

    setup db scripts

--- create_schema.py
+++ create_schema.py
@@ -3,7 +3,7 @@
 create the tables from my models
 '''
 
-from models import db,app
+from models import storage,app
 from models.user import User 
 from models.review import Review
 from models.recipe import Recipe
@@ -12,5 +12,4 @@ with app.app_context():
     ''' access the app_context allow to intract with app and get required config'''
 
     # create the tables again and the strucure of my models:
-    db.drop_all()
-    db.create_all()
+    storage.reload()
