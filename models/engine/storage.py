7d17b34975bf3a29aa9d558b4c471c01a9fa3096
sozoia <zfzg2232@gmail.com>
2024-09-12 22:39:04 +0300

    add comment

--- models/engine/storage.py
+++ models/engine/storage.py
@@ -59,7 +59,8 @@ class TheStorage:
 
     def reload(self):
         """reloads data from the database"""
-        Base.metadata.create_all(self.__engine)
+        # Base.metadata.drop_all(self.__engine)
+        Base.metadata.create_all(self.__engine) #  it won't update or modify existing tables just create the new tables. 
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session
