68be807bdf94a1e3c6d23abc0c85ec2a07aa0f67
sozoia <zfzg2232@gmail.com>
2024-09-14 02:01:04 +0300

    remove venv from repo

--- create_schema.py
+++ create_schema.py
@@ -7,7 +7,7 @@ from models import storage,app
 from models.user import User 
 from models.review import Review
 from models.recipe import Recipe
-
+from models.base_model import Base
 with app.app_context():
     ''' access the app_context allow to intract with app and get required config'''
 
